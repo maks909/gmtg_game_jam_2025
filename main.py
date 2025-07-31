e4f1e04cf4a9bba743e976577c43905fe762008f
Maxim <kuzalkovmaxim@gmail.com>
2025-07-31 13:05:13 +0200

    Added player movement logic

--- main.py
+++ main.py
@@ -142,15 +142,16 @@ class Player(arcade.TextureAnimationSprite):
 
         self.center_x=300
         self.center_y=300
-        self.scale=1.0
+        self.scale=5
         
         self.run_left_animation = arcade.TextureAnimation(run_left_frames)
         self.run_right_animation = arcade.TextureAnimation(run_right_frames)
-        self.animation = self.run_left_animation
 
-    def draw(self, delta_time: float = 1/60):
-        self.center_x += 200
-        self.texture = self.animation.update(delta_time)
+        self.stand_left_animation = arcade.TextureAnimation([arcade.TextureKeyframe(run_left_textures[0])])
+        self.stand_right_animation = arcade.TextureAnimation([arcade.TextureKeyframe(run_right_textures[0])])
+
+        self.animation = self.stand_left_animation
+
 
 class PlatformerView(arcade.View):
     def __init__(self):
@@ -158,14 +159,38 @@ class PlatformerView(arcade.View):
         self.sprite_list = arcade.SpriteList()
         self.player = Player()
         self.sprite_list.append(self.player)
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player)
     
     def on_draw(self):
         self.clear()
         self.sprite_list.draw()
     
+    def on_update(self, delta_time):
+        # Update sprite positions
+        self.sprite_list.update()
+        
+        self.sprite_list.update_animation()
+        
+        # Update physics engine if needed
+        self.physics_engine.update()
+    
     def on_key_press(self, key, modifiers):
-        if key == arcade.key.ESCAPE:
+        if key == arcade.key.LEFT:
+            self.player.animation = self.player.run_left_animation
+            self.player.change_x = -10
+        elif key == arcade.key.RIGHT:
+            self.player.animation = self.player.run_right_animation
+            self.player.change_x = 10
+        elif key == arcade.key.ESCAPE:
             self.window.show_view(PauseGUIView())
+    
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.LEFT:
+            self.player.change_x = 0
+            self.player.animation = self.player.stand_left_animation
+        elif key == arcade.key.RIGHT:
+            self.player.change_x = 0
+            self.player.animation = self.player.stand_right_animation
     
     def on_show_view(self) -> None:
         pass
